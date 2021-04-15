6b50271c1b1bf28ad33e99be5dfb610e5710ced9
Gabo Gilabert <ggilaber@microsoft.com>
2021-04-15 10:06:38 -0400

    First pass for the sample

---
+++ ProactiveBot/Bots/ProactiveBot.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace ProactiveBot.Bots
+{
+    public class ProactiveBot : ActivityHandler
+    {
+        // Message to send to users when the bot receives a Conversation Update event
+        private const string WelcomeMessage = "Welcome to the Proactive Bot sample";
+        private const string NotifyMessage = "Navigate to {0}api/notify?user={1}&message=Test to proactively message the user.";
+
+        // Dependency injected dictionary for storing ConversationReference objects used in NotifyController to proactively message users
+        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferencesStore;
+
+        private readonly Uri _serverUrl;
+
+        public ProactiveBot(IHttpContextAccessor httpContextAccessor, ConcurrentDictionary<string, ConversationReference> conversationReferencesStore)
+        {
+            _conversationReferencesStore = conversationReferencesStore;
+            _serverUrl = new Uri($"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host.Value}");
+        }
+
+        protected override Task OnConversationUpdateActivityAsync(ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            AddConversationReference(turnContext.Activity as Activity);
+
+            return base.OnConversationUpdateActivityAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                // Greet anyone that was not the target (recipient) of this message.
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    // Remember the conversation reference so we can send proactive messages to the user.
+                    AddConversationReference(turnContext.Activity as Activity);
+
+                    // Greet the user and render message with continuation link.
+                    await turnContext.SendActivityAsync(MessageFactory.Text(WelcomeMessage), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text(string.Format(NotifyMessage, _serverUrl, turnContext.Activity.From.Id)), cancellationToken);
+                }
+            }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // Echo back what the user said and render message with continuation link.
+            await turnContext.SendActivityAsync(MessageFactory.Text($"You sent '{turnContext.Activity.Text}'"), cancellationToken);
+            await turnContext.SendActivityAsync(MessageFactory.Text(string.Format(NotifyMessage, _serverUrl, turnContext.Activity.From.Id)), cancellationToken);
+        }
+
+        /// <summary>
+        /// Store the conversation reference for that user so we send a proactive message. 
+        /// </summary>
+        private void AddConversationReference(Activity activity)
+        {
+            var conversationReference = activity.GetConversationReference();
+            _conversationReferencesStore.AddOrUpdate(conversationReference.User.Id, conversationReference, (key, newValue) => conversationReference);
+        }
+    }
+}
---
+++ ProactiveBot/Controllers/NotifyController.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+namespace ProactiveBot.Controllers
+{
+    [Route("api/notify")]
+    [ApiController]
+    public class NotifyController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly string _appId;
+        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferencesStore;
+
+        public NotifyController(IBotFrameworkHttpAdapter adapter, IConfiguration configuration, ConcurrentDictionary<string, ConversationReference> conversationReferencesStore)
+        {
+            _adapter = adapter;
+            _conversationReferencesStore = conversationReferencesStore;
+            _appId = configuration["MicrosoftAppId"] ?? string.Empty;
+        }
+
+        public async Task<IActionResult> Get(string user, string message)
+        {
+            // Try to get a conversation reference for that user from the conversation reference store.
+            _conversationReferencesStore.TryGetValue(user, out var continuationParameters);
+
+            // Sample: this is how you would manually create a conversation reference for ACS to send an SMS to the user:
+            // Note that this is only possible for SMS, for the other channels, the user needs to talk to the channel at least 
+            // once so we can store a conversation reference for that user.
+            //var continuationParameters = new ConversationReference
+            //{
+            //    User = new ChannelAccount(userSmsNumber),
+            //    Bot = new ChannelAccount(config.GetSection("acsSmsAdapterSettings")["AcsPhoneNumber"], "bot"),
+            //    Conversation = new ConversationAccount(false, null, userSmsNumber),
+            //    ChannelId = "ACS_SMS"
+            //};
+
+            if (continuationParameters == null)
+            {
+                // We didn't find a conversation reference to the user.
+                return new ContentResult
+                {
+                    Content = $"<html><body><h1>No messages sent</h1> <br/>There are no conversations registered to receive proactive messages for {user}.</body></html>",
+                    ContentType = "text/html",
+                    StatusCode = (int)HttpStatusCode.OK,
+                };
+            }
+
+            Exception exception = null;
+            try
+            {
+                // This anonymous function will be called from ContinueConversationAsync
+                async Task ContinuationBotCallback(ITurnContext context, CancellationToken cancellationToken)
+                {
+                    await context.SendActivityAsync($"Got proactive message \"{message}\" for user: {user}", cancellationToken: cancellationToken);
+                }
+
+                // Continue the conversation with the proactive message
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, continuationParameters, ContinuationBotCallback, default);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            // Let the caller know a proactive messages have been sent
+            return new ContentResult
+            {
+                Content = $"<html><body><h1>Proactive messages have been sent</h1> <br/> Timestamp: {DateTime.Now} <br /> Exception: {exception}</body></html>",
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+            };
+        }
+    }
+}
